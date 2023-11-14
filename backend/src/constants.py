ca7c3a50eec5070d6ea77822a3f947d78011d857
RWallan <3am.richardwallan@gmail.com>
2023-11-14 13:40:59 -0300

    feat: Texto do Zen do Python

---
+++ backend/src/constants.py
@@ -0,0 +1,26 @@
+ZEN = """<p>Olá, {} {}</p>
+
+<p>Aqui está o seu <b>Zen Do Python</b>.</p>
+<br>
+<p>Beautiful is better than ugly.</p>
+<p>Explicit is better than implicit.</p>
+<p>Simple is better than complex.</p>
+<p>Complex is better than complicated.</p>
+<p>Flat is better than nested.</p>
+<p>Sparse is better than dense.</p>
+<p>Readability counts.</p>
+<p>Special cases aren't special enough to break the rules.</p>
+<p>Although practicality beats purity.</p>
+<p>Errors should never pass silently.</p>
+<p>Unless explicitly silenced.</p>
+<p>In the face of ambiguity, refuse the temptation to guess.</p>
+<p>There should be one-- and preferably only one --obvious way to do it.</p>
+<p>Although that way may not be obvious at first unless you're Dutch.</p>
+<p>Now is better than never.</p>
+<p>Although never is often better than *right* now.</p>
+<p>If the implementation is hard to explain, it's a bad idea.</p>
+<p>If the implementation is easy to explain, it may be a good idea.</p>
+<p>Namespaces are one honking great idea -- let's do more of those!</p>
+<br>
+<p>Você também pode conferir diretamente pela <a href="https://peps.python.org/pep-0020/">PEP-20.</a></p>
+""" # noqa E501
